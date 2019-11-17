2a9d516c05a208255d2f39aa5fa818c6fdf727a0
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-17 00:02:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,14 +5,14 @@ end
 introduction("Damian")
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in {language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language("Damian", "Ruby")
 introduction_with_language("Osvaldo", "Python")
 
 def introduction_with_language_optional(name, language="Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in {language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language_optional("Juan")
